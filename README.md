50e8dbb10696d51de2e613dee560662be580cbd4
mxmlnkn <mxmlnkn@github.de>
2019-02-20 17:09:51 +0100

    add first working version

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# ratarmount
-Random Access Read-Only Tar Mount
+# Random Access Read-Only Tar Mount (Ratarmount)
+
+Combines the random access indexing idea from [tarindexer](https://github.com/devsnd/tarindexer) and then mounts the tar using [fusepy](https://github.com/fusepy/fusepy) for easy read-only access just like [archivemount](https://github.com/cybernoid/archivemount/). It also will mount TARs inside TARs inside TARs, ... recursively into folders of the same name, which is useful for the ImageNet data set.
+
+# Requirements
+
+ - Python3
+ - fusepy
+ 
+E.g. on Debian-like systems these can be installed with:
+
+```bash
+sudo apt-get update
+sudo apt-get install python3
+pip3 --user fusepy
+```
+
+# Usage
+
+    python3 ratarmount.py <path to tar> [<mount path>]
+
+Index files are if possible created to / if existing loaded from these file locations in order:
+
+  - `<path to tar>.index.pickle`
+  - `~/.tarmount/<path to tar: '/' -> '_'>.index.pickle`
+
+# The Problem
+
+You downloaded a large TAR file from the internet, for example the [1.31TB](http://academictorrents.com/details/564a77c1e1119da199ff32622a1609431b9f1c47) large [ImageNet](http://image-net.org/), and you now want to use it but lack the space, time, or a file system fast enough to extract all the 14.2 million image files.
+
+## Partial Solutions
+
+### Archivemount
+
+Archivemount[https://github.com/cybernoid/archivemount/] does not seem to support random access in version 0.8.7 and also mounting seems to have performance issues:
+
+  - Mounting the 6.5GB ImageNet Large-Scale Visual Recognition Challenge 2012 validation data set, and then testing the speed with: `time cat mounted/ILSVRC2012_val_00049975.JPEG | wc -c` takes 250ms for archivemount and 2ms for ratarmount.
+  - Trying to mount the 150GB [ILSVRC object localization data set](https://www.kaggle.com/c/imagenet-object-localization-challenge) containing 2 million images was given up upon after 2 hours. Ratarmount takes 45min to create the index and <10s for loading an already created index and mounting it. In contrast, archivemount will take the same amount of time even for subsequent mounts.
+
+### Tarindexer
+
+[Tarindex](https://github.com/devsnd/tarindexer) is a command line to tool written in Python which can create index files and then use the index file to extract single files from the tar fast. However, it also has some caveats which ratarmount tries to solve:
+
+  - It only works with single files, meaning it would be necessary to loop over the extract-call. But this would require loading the possibly quite large tar index file into memory each time. For example for ImageNet, the resulting index file is hundreds of MB large. Also, extracting directories will be a hassle.
+  - It's difficult to integrate tarindexer into other production environments. Ratarmount instead uses FUSE to mount the TAR as a folder readable by any other programs requiring access to the contained data.
+  - Can't handle TARs recursively. In order to extract files inside a TAR which itself is inside a TAR, the packed TAR first needs to be extracted.
+
+### TAR Browser
+
+I didn't find out about [TAR Browser](https://github.com/tomorrow-nf/tar-as-filesystem/) before I finished the ratarmount script. That's also one of it's cons:
+
+  - Hard to find. I don't seem to be the only one who has trouble finding it as it has zero stars on Github after 4 years compared to 29 stars for tarindexer after roughly the same amount of time.
+  - Hassle to set up. Needs compilation and I gave up when I was instructed to set up a MySQL database for it to use. Btw, the setup instructions are not on its Github but [here](https://web.wpi.edu/Pubs/E-project/Available/E-project-030615-133259/unrestricted/TARBrowserFinal.pdf).
+  - Doesn't seem to support recursive TAR mounting. I didn't test it because of the MysQL dependency but the code does not seem to have logic for recursive mounting.
+
+Pros:
+  - supports bz2- and xz-compressed TAR archives
+
+## The Solution
+
+Ratarmount creates an index file with file names, ownership, permission flags, and offset information to be stored at the TAR file's location or inside `~/.ratarmount/` and then offers a FUSE mount integration for easy access to the files.
+
+The test for the ImageNet data set is promising:
+
+  - TAR size: 1.31TB
+  - Contains TARs: yes
+  - Files in TAR: ~26 000
+  - Files in TAR (including recursively in contained TARs): 14.2 million
+  - Index creation (first mounting): 4 hours
+  - Index size: 1GB
+  - Index loading (subsequent mounting): 40s
+  - Reading a 40kB file: 100ms (first time) and 4ms (subsequent times)
+
+The reading time for a small file simply verifies the random access by using file seek to be working. The difference between the first read and subsequent reads is not because of ratarmount but because of operating system and file system caches.
---
+++ ratarmount.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python3
+
+import os, re, sys, stat, tarfile, pickle, fuse
+from collections import namedtuple
+from timeit import default_timer as timer
+
+printDebug = 1
+
+def overrides( parentClass ):
+    def overrider( method ):
+        assert( method.__name__ in dir( parentClass ) )
+        return method
+    return overrider
+
+
+FileInfo = namedtuple( "FileInfo", "offset size mtime mode type linkname uid gid istar" )
+
+class IndexedTar:
+    def __init__( self, pathToTar = None, fileObject = None, writeIndex = False ):
+        self.tarFileName = pathToTar
+        self.fileIndex = {}
+        self.dirIndex = {}
+
+        if fileObject is not None:
+            if writeIndex:
+                print( "Can't write out index for file object input. Ignoring this option." )
+            self.createIndex( fileObject )
+        else:
+            self.indexFileName = self.tarFileName + ".index.pickle"
+            self.cacheFolder = os.path.expanduser( "~/.ratarmount" )
+            self.altIndexFileName = self.cacheFolder + "/" + self.tarFileName.replace( "/", "_" ) + ".index.pickle"
+
+            for indexPath in [ self.indexFileName, self.altIndexFileName ]:
+                if not self.dirIndex and not self.fileIndex and os.path.isfile( indexPath ):
+                    if os.path.getsize( indexPath ) > 0:
+                        self.loadIndex( indexPath )
+                    else:
+                        os.remove( indexPath )
+
+            if not self.dirIndex and not self.fileIndex:
+                with open( self.tarFileName, 'rb' ) as file:
+                    self.createIndex( file )
+
+                if writeIndex:
+                    try:
+                        try:
+                            f = open( self.indexFileName, 'wb' )
+                            f.close()
+                        except IOError:
+                            if not os.path.exists( self.cacheFolder ):
+                                os.mkdir( self.cacheFolder )
+                            self.indexFileName = self.altIndexFileName
+
+                        self.writeIndex( self.indexFileName )
+                    except IOError:
+                        print( "[Info] Could not write TAR index to file. Subsequent mounts might be slow!" )
+
+    def isDir( self, path ):
+        return path in self.dirIndex
+
+    def getDirInfo( self, path ):
+        if path in self.dirIndex:
+            return self.dirIndex[path]
+        return None
+
+    def getFileInfo( self, path, listDir = False ):
+        if not listDir and path in self.dirIndex:
+            return self.dirIndex[path]
+
+        p = self.fileIndex
+        for name in path.split( '/' ):
+            if not name:
+                continue
+
+            if not name in p:
+                return
+
+            p = p[name]
+        return p
+
+    def exists( self, path ):
+        return self.isDir( path ) or isinstance( self.getFileInfo( path ), FileInfo )
+
+    @staticmethod
+    def setFileInfo( fileIndex, path, fileInfo ):
+        p = fileIndex
+        for name in path.split( '/' )[:-1]:
+            if not name:
+                continue
+
+            if not name in p:
+                p = p.setdefault( name, {} )
+            else:
+                p = p[name]
+
+        fileName = path.split( '/' )[-1]
+        p.update( { fileName : fileInfo } )
+
+
+    def createIndex( self, fileObject ):
+        if printDebug >= 1:
+            print( "Creating offset dictionary for", "<file object>" if self.tarFileName is None else self.tarFileName, "..." )
+        t0 = timer()
+
+        self.dirIndex = {}
+        self.fileIndex = {}
+        loadedTarFile = tarfile.open( fileobj = fileObject )
+        for tarInfo in loadedTarFile:
+            mode = tarInfo.mode
+            if tarInfo.isdir() : mode |= stat.S_IFDIR
+            if tarInfo.isfile(): mode |= stat.S_IFREG
+            if tarInfo.issym() : mode |= stat.S_IFLNK
+            if tarInfo.ischr() : mode |= stat.S_IFCHR
+            if tarInfo.isfifo(): mode |= stat.S_IFIFO
+            fileInfo = FileInfo(
+                offset   = tarInfo.offset_data,
+                size     = tarInfo.size       ,
+                mtime    = tarInfo.mtime      ,
+                mode     = mode               ,
+                type     = tarInfo.type       ,
+                linkname = tarInfo.linkname   ,
+                uid      = tarInfo.uid        ,
+                gid      = tarInfo.gid        ,
+                istar    = False
+            )
+
+            # open contained tars for recursive mounting
+            indexedTar = None
+            if tarInfo.isfile() and tarInfo.name.endswith( ".tar" ):
+                oldPos = fileObject.tell()
+                if oldPos != tarInfo.offset_data:
+                    fileObject.seek( tarInfo.offset_data )
+                indexedTar = IndexedTar( tarInfo.name, fileObject = fileObject, writeIndex = False )
+                fileObject.seek( fileObject.tell() ) # might be especially necessary if the .tar is not actually a tar!
+
+            path = "/" + tarInfo.name
+
+            if indexedTar is not None and ( indexedTar.dirIndex or indexedTar.fileIndex ):
+                # actually apply the recursive tar mounting
+                extractedName = re.sub( r"\.tar$", "", path )
+                if not self.exists( extractedName ):
+                    path = extractedName
+
+                mountMode = ( fileInfo.mode & 0o777 ) | stat.S_IFDIR
+                if mountMode & stat.S_IRUSR != 0: mountMode |= stat.S_IXUSR
+                if mountMode & stat.S_IRGRP != 0: mountMode |= stat.S_IXGRP
+                if mountMode & stat.S_IROTH != 0: mountMode |= stat.S_IXOTH
+                fileInfo = fileInfo._replace( mode = mountMode, istar = True )
+
+                if self.exists( path ):
+                    print( "[Warning]", path, "already exists in database and will be overwritten!" )
+
+                for dir, info in indexedTar.dirIndex.items():
+                    self.dirIndex[path + dir] = info
+                self.dirIndex[path] = fileInfo
+                self.setFileInfo( self.fileIndex, path, indexedTar.fileIndex )
+            else:
+                if self.exists( path ):
+                    fileInfo = self.getFileInfo( path )
+                    if fileInfo.istar:
+                        self.setFileInfo( self.fileIndex, path + ".tar", fileInfo )
+                        # no need to delete old entry. Will be overwritten anyway
+                        self.dirIndex[path + ".tar"] = self.dirIndex.pop( path )
+                    else:
+                        print( "[Warning]", path, "already exists in database and will be overwritten!" )
+
+                if tarInfo.isdir():
+                    self.dirIndex[path] = fileInfo
+                    self.setFileInfo( self.fileIndex, path, {} )
+                else:
+                    self.setFileInfo( self.fileIndex, path, fileInfo )
+
+        # add parent folders if they were left out in the tar info
+        dirsToAdd = {}
+        for path, info in self.dirIndex.items():
+            while '/' in path:
+                path = re.sub( r"/+[^/]*$", "", path )
+                if path not in dirsToAdd and path not in self.dirIndex:
+                    dirsToAdd[path] = FileInfo( info.offset, 0, info.mtime, 0o555 | stat.S_IFDIR, info.type,
+                                                "", info.uid, info.gid, False )
+        self.dirIndex.update( dirsToAdd )
+
+        t1 = timer()
+        if printDebug >= 1:
+            print( "Creating offset dictionary for", "<file object>" if self.tarFileName is None else self.tarFileName, "took {:.2f}s".format( t1 - t0 ) )
+
+    def writeIndex( self, outFileName ):
+        with open( outFileName, 'wb' ) as outFile:
+            pickle.dump( ( self.dirIndex, self.fileIndex ), outFile )
+
+    def loadIndex( self, indexFileName ):
+        if printDebug >= 1:
+            print( "Loading offset dictionary from", indexFileName, "..." )
+        t0 = timer()
+
+        with open( indexFileName, 'rb' ) as indexFile:
+            self.dirIndex, self.fileIndex = pickle.load( indexFile )
+
+        if printDebug >= 2:
+            def countDictEntries( d ):
+                n = 0
+                for key, value in d.items():
+                    n += countDictEntries( value ) if type( value ) is dict else 1
+                return n
+            print( "Files:", countDictEntries( self.fileIndex ), "Dirs:", len( self.dirIndex ) )
+
+        # add parent folders if they were left out in the tar info
+        dirsToAdd = {}
+        for path, info in self.dirIndex.items():
+            while '/' in path:
+                path = re.sub( r"/+[^/]*$", "", path )
+                if path not in dirsToAdd and path not in self.dirIndex:
+                    dirsToAdd[path] = FileInfo( info.offset, 0, info.mtime, 0o555 | stat.S_IFDIR, info.type,
+                                                "", info.uid, info.gid, False )
+        self.dirIndex.update( dirsToAdd )
+
+        t1 = timer()
+        if printDebug >= 1:
+            print( "Loading offset dictionary from", indexFileName, "took {:.2f}s".format( t1 - t0 ) )
+
+class TarMount( fuse.Operations ):
+    def __init__( self, pathToMount ):
+        self.tarFileName = pathToMount
+        self.tarFile = open( self.tarFileName, 'rb' )
+        self.indexedTar = IndexedTar( self.tarFileName, writeIndex = True )
+
+        # make the mount point read only and executable if readable, i.e., allow directory listing
+        tarStats = os.stat( self.tarFileName )
+        # clear higher bits like S_IFREG and set the directory bit instead
+        mountMode = ( tarStats.st_mode & 0o777 ) | stat.S_IFDIR
+        if mountMode & stat.S_IRUSR != 0: mountMode |= stat.S_IXUSR
+        if mountMode & stat.S_IRGRP != 0: mountMode |= stat.S_IXGRP
+        if mountMode & stat.S_IROTH != 0: mountMode |= stat.S_IXOTH
+        self.indexedTar.dirIndex[ "/" ] = fileInfo = FileInfo(
+            offset   = 0                ,
+            size     = tarStats.st_size ,
+            mtime    = tarStats.st_mtime,
+            mode     = mountMode        ,
+            type     = tarfile.DIRTYPE  ,
+            linkname = ""               ,
+            uid      = tarStats.st_uid  ,
+            gid      = tarStats.st_gid  ,
+            istar    = True
+        )
+
+        if printDebug >= 3:
+            print( "Loaded Dir Index:", self.indexedTar.dirIndex )
+            print( "Loaded File Index:", self.indexedTar.fileIndex )
+
+    @overrides( fuse.Operations )
+    def getattr( self, path, fh = None ):
+        if printDebug >= 2:
+            print( "[getattr( path =", path, ", fh =", fh, ")]" )
+
+
+        fileInfo = self.indexedTar.getFileInfo( path )
+        if not isinstance( fileInfo, FileInfo ):
+            if printDebug >= 2:
+                print( "Could not find path:", path )
+            raise fuse.FuseOSError( fuse.errno.EROFS )
+
+        # dictionary keys: https://pubs.opengroup.org/onlinepubs/007904875/basedefs/sys/stat.h.html
+        statDict = dict( ( "st_" + key, getattr( fileInfo, key ) ) for key in ( 'size', 'mtime', 'mode', 'uid', 'gid' ) )
+        # signal that everything was mounted read-only
+        statDict['st_mode'] &= ~( stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH )
+        statDict['st_mtime'] = int( statDict['st_mtime'] )
+        statDict['st_nlink'] = 2
+
+        return statDict
+
+    @overrides( fuse.Operations )
+    def readdir( self, path, fh ):
+        if printDebug >= 2:
+            print( "[readdir( path =", path, ", fh =", fh, ")] getFileInfo.keys:",
+                   self.indexedTar.getFileInfo( path, listDir = True ).keys() )
+
+        # we only need to return these special directories. FUSE automatically expands these and will not ask
+        # for paths like /../foo/./../bar, so we don't need to worry about cleaning such paths
+        yield '.'
+        yield '..'
+
+        for key in self.indexedTar.getFileInfo( path, listDir = True ).keys():
+            yield key
+
+    @overrides( fuse.Operations )
+    def readlink( self, path ):
+        fileInfo = self.indexedTar.getFileInfo( path )
+        if not isinstance( fileInfo, FileInfo ):
+            raise fuse.FuseOSError( fuse.errno.EROFS )
+
+        pathname = fileInfo.linkname
+        if pathname.startswith( "/" ):
+            return os.path.relpath( pathname, self.root )
+        else:
+            return pathname
+
+    @overrides( fuse.Operations )
+    def read( self, path, length, offset, fh ):
+        if printDebug >= 2:
+            print( "[read( path =", path, ", length =", length, ", offset =", offset, ",fh =", fh, ")] path:", path )
+
+        fileInfo = self.indexedTar.getFileInfo( path )
+        if not isinstance( fileInfo, FileInfo ):
+            raise fuse.FuseOSError( fuse.errno.EROFS )
+
+        self.tarFile.seek( fileInfo.offset + offset, os.SEEK_SET )
+        return self.tarFile.read( length )
+
+if __name__ == '__main__':
+    if len( sys.argv ) < 2 or "--help" in sys.argv or "-h" in sys.argv:
+        print( """Usage:
+    ratarmount <path to tar> [<mount path>]
+
+If no mount path is specified, then the tar will be mounted to a folder of the same name but without a file extension.
+TAR files contained inside the tar and even TARs in TARs in TARs will be mounted recursively at folders of the same name barred the file extension '.tar'.
+
+In order to reduce the mounting time, the created index for random access to files inside the tar will be saved to <path to tar>.index.pickle. If it can't be saved there, it will be saved in ~/.ratarmount/<path to tar: '/' -> '_'>.index.pickle.
+""" )
+
+    tarToMount = sys.argv[1]
+    mountPath = sys.argv[2] if len( sys.argv ) >= 3 else os.path.splitext( tarToMount )[0]
+    mountPathWasCreated = False
+    if not os.path.exists( mountPath ):
+        os.mkdir( mountPath )
+
+    foreground = False
+    fuse.FUSE( operations = TarMount( pathToMount = tarToMount ), mountpoint = mountPath, foreground = foreground )
+    if mountPathWasCreated and foreground:
+        os.rmdir( mountPath )
