6369b5d191220ee0799f98c3d2618d65bf577614
mxmlnkn <mxmlnkn@github.de>
2019-05-19 12:26:05 +0200

    add tests for recursive TARs

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
 echoerr() { echo "$@" 1>&2; }
 
 error=0
@@ -12,21 +14,21 @@ checkFileInTAR()
     local mountFolder="$( mktemp -d )"
 
     # try with index recreation
-    python3 ratarmount.py -c "$archive" "$mountFolder" &>/dev/null
+    python3 ratarmount.py -c --recursive "$archive" "$mountFolder" &>/dev/null
     checksum="$( md5sum "$mountFolder/$fileInTar" 2>/dev/null | sed 's| .*||' )"
     if test "$checksum" != "$correctChecksum"; then
-        echoerr 'TEST FAILED!'
         echoerr "File sum of '$fileInTar' in mounted TAR '$archive' does not match"'!'
+        echoerr 'TEST FAILED!'
         return 1
     fi
     fusermount -u "$mountFolder" &>/dev/null
 
     # retry without forcing index recreation
-    python3 ratarmount.py "$archive" "$mountFolder" &>/dev/null
+    python3 ratarmount.py --recursive "$archive" "$mountFolder" &>/dev/null
     checksum="$( md5sum "$mountFolder/$fileInTar" 2>/dev/null | sed 's| .*||' )"
     if test "$checksum" != "$correctChecksum"; then
-        echoerr 'TEST FAILED!'
         echoerr "File sum of '$fileInTar' in mounted TAR '$archive' does not match"'!'
+        echoerr 'TEST FAILED!'
         return 1
     fi
     fusermount -u "$mountFolder" &>/dev/null
@@ -36,11 +38,21 @@ checkFileInTAR()
     return 0
 }
 
+rm -f tests/*.index.pickle
+
 checkFileInTAR tests/single-file.tar bar d3b07384d113edec49eaa6238ad5ff00
 checkFileInTAR tests/single-file-with-leading-dot-slash.tar bar d3b07384d113edec49eaa6238ad5ff00
 checkFileInTAR tests/folder-with-leading-dot-slash.tar foo/bar 2b87e29fca6ee7f1df6c1a76cb58e101
 checkFileInTAR tests/folder-with-leading-dot-slash.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
 checkFileInTAR tests/single-nested-file.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
 checkFileInTAR tests/single-nested-folder.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
+
+checkFileInTAR tests/nested-tar.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
+checkFileInTAR tests/nested-tar.tar foo/lighter/fighter/bar 2b87e29fca6ee7f1df6c1a76cb58e101
+
+checkFileInTAR tests/nested-tar-with-overlapping-name.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
+checkFileInTAR tests/nested-tar-with-overlapping-name.tar foo/fighter.tar/fighter/bar 2b87e29fca6ee7f1df6c1a76cb58e101
+
+rm -f tests/*.index.pickle
 
 exit $error
--- tests/create-tests.sh
+++ tests/create-tests.sh
@@ -14,8 +14,14 @@ recreateArchive()
     tar -c -f "$newArchive" --owner=user --group=group "$@"
 )
 
+# These archives are mostly only different in what kind of TAR they produce but don't differ with their actual contents.
+# So, for these test TARs the creation command is more important than their contents.
+# That's why their creation commands are indirectly documented in this shell script to recreate the archives from
+# their extracted contents.
+# Archives simply created by, e.g., "tar cf contents{.tar,}" don't have to be listed here
 recreateArchive 'single-file.tar' bar
 recreateArchive 'single-file-with-leading-dot-slash.tar' ./bar
 recreateArchive 'folder-with-leading-dot-slash.tar' ./
 recreateArchive 'single-nested-file.tar' foo/fighter/ufo
 recreateArchive 'single-nested-folder.tar' foo/fighter/
+recreateArchive 'file-existing-as-non-link-and-link.tar' foo/fighter/ foo/fighter/ufo
