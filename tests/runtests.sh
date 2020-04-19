ca06a4276a3d2ded6e0e3d3a109b41daa47e4cbe
mxmlnkn <mxmlnkn@github.de>
2020-04-19 21:48:00 +0200

    [refactor] Separate legacy tests from SQLite backend tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -332,59 +332,70 @@ pylint --disable=C0326,C0103 ratarmount.py > pylint.log
 
 rm -f tests/*.index.*
 
-for type in sqlite custom pickle2 pickle3 cbor msgpack rapidjson ujson simplejson; do
+tests=(
+    d3b07384d113edec49eaa6238ad5ff00 tests/single-file.tar                        bar
+    d3b07384d113edec49eaa6238ad5ff00 tests/single-file-with-leading-dot-slash.tar bar
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/folder-with-leading-dot-slash.tar      foo/bar
+    2709a3348eb2c52302a7606ecf5860bc tests/folder-with-leading-dot-slash.tar      foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/single-nested-file.tar                 foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/single-nested-folder.tar               foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/nested-tar.tar                         foo/fighter/ufo
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar.tar                         foo/lighter.tar/fighter/bar
+    2709a3348eb2c52302a7606ecf5860bc tests/nested-tar-with-overlapping-name.tar   foo/fighter/ufo
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar-with-overlapping-name.tar   foo/fighter.tar/fighter/bar
+    832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   01.sparse1.bin
+    832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   02.normal1.bin
+    832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   03.sparse1.bin
+)
+
+# SQLite backend tests
+
+checkLinkInTAR tests/symlinks.tar foo ../foo
+checkLinkInTAR tests/symlinks.tar python /usr/bin/python
+
+echoerr "=== Testing SQLite Backend ==="
+
+for (( iTest = 0; iTest < ${#tests[@]}; iTest += 3 )); do
+    checkFileInTAR sqlite "${tests[iTest+1]}" "${tests[iTest+2]}" "${tests[iTest]}"
+
+    tmpBz2=$( mktemp --suffix='.tar.bz2' )
+    bzip2 --keep --stdout "${tests[iTest+1]}" > "$tmpBz2"
+    checkFileInTAR sqlite "$tmpBz2" "${tests[iTest+2]}" "${tests[iTest]}"
+    'rm' -- "$tmpBz2"
+
+    tmpGz=$( mktemp --suffix='.tar.gz' )
+    gzip --keep --stdout "${tests[iTest+1]}" > "$tmpGz"
+    checkFileInTAR sqlite "$tmpGz" "${tests[iTest+2]}" "${tests[iTest]}"
+    'rm' -- "$tmpGz"
+done
+
+checkFileInTARPrefix '' tests/single-nested-file.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
+checkFileInTARPrefix foo tests/single-nested-file.tar fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
+checkFileInTARPrefix foo/fighter tests/single-nested-file.tar ufo 2709a3348eb2c52302a7606ecf5860bc
+
+# Deprecated serialization backend tests
+
+tests=(
+    d3b07384d113edec49eaa6238ad5ff00 tests/single-file.tar                        bar
+    d3b07384d113edec49eaa6238ad5ff00 tests/single-file-with-leading-dot-slash.tar bar
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/folder-with-leading-dot-slash.tar      foo/bar
+    2709a3348eb2c52302a7606ecf5860bc tests/folder-with-leading-dot-slash.tar      foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/single-nested-file.tar                 foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/single-nested-folder.tar               foo/fighter/ufo
+    2709a3348eb2c52302a7606ecf5860bc tests/nested-tar.tar                         foo/fighter/ufo
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar.tar                         foo/lighter.tar/fighter/bar
+    2709a3348eb2c52302a7606ecf5860bc tests/nested-tar-with-overlapping-name.tar   foo/fighter/ufo
+    2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar-with-overlapping-name.tar   foo/fighter.tar/fighter/bar
+)
+
+for type in custom pickle2 pickle3 cbor msgpack rapidjson ujson simplejson; do
     compressions=( '' '.gz' '.lz4' )
-    if [[ "$type" == 'sqlite' ]]; then compressions=( '' ); fi
     for compression in "${compressions[@]}"; do
         echoerr "=== Testing Serialization Backend: ${type}${compression} ==="
-
-        checkLinkInTAR tests/symlinks.tar foo ../foo
-        checkLinkInTAR tests/symlinks.tar python /usr/bin/python
-
-        tests=(
-            d3b07384d113edec49eaa6238ad5ff00 tests/single-file.tar                        bar
-            d3b07384d113edec49eaa6238ad5ff00 tests/single-file-with-leading-dot-slash.tar bar
-            2b87e29fca6ee7f1df6c1a76cb58e101 tests/folder-with-leading-dot-slash.tar      foo/bar
-            2709a3348eb2c52302a7606ecf5860bc tests/folder-with-leading-dot-slash.tar      foo/fighter/ufo
-            2709a3348eb2c52302a7606ecf5860bc tests/single-nested-file.tar                 foo/fighter/ufo
-            2709a3348eb2c52302a7606ecf5860bc tests/single-nested-folder.tar               foo/fighter/ufo
-            2709a3348eb2c52302a7606ecf5860bc tests/nested-tar.tar                         foo/fighter/ufo
-            2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar.tar                         foo/lighter.tar/fighter/bar
-            2709a3348eb2c52302a7606ecf5860bc tests/nested-tar-with-overlapping-name.tar   foo/fighter/ufo
-            2b87e29fca6ee7f1df6c1a76cb58e101 tests/nested-tar-with-overlapping-name.tar   foo/fighter.tar/fighter/bar
-        )
-
-        # Sparse file support is not backported to the old serializers
-        if [[ $type == 'sqlite' ]]; then
-            tests+=(
-                832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   01.sparse1.bin
-                832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   02.normal1.bin
-                832c78afcb9832e1a21c18212fc6c38b tests/gnu-sparse-files.tar                   03.sparse1.bin
-            )
-        fi
-
         for (( iTest = 0; iTest < ${#tests[@]}; iTest += 3 )); do
             checkFileInTAR "${type}${compression}" "${tests[iTest+1]}" "${tests[iTest+2]}" "${tests[iTest]}"
-            # For SQLite backend, check with BZip2 compression
-            if [[ $type == 'sqlite' ]]; then
-                tmpBz2=$( mktemp --suffix='.tar.bz2' )
-                bzip2 --keep --stdout "${tests[iTest+1]}" > "$tmpBz2"
-                checkFileInTAR "${type}${compression}" "$tmpBz2" "${tests[iTest+2]}" "${tests[iTest]}"
-                'rm' -- "$tmpBz2"
-
-                tmpGz=$( mktemp --suffix='.tar.gz' )
-                gzip --keep --stdout "${tests[iTest+1]}" > "$tmpGz"
-                checkFileInTAR "${type}${compression}" "$tmpGz" "${tests[iTest+2]}" "${tests[iTest]}"
-                'rm' -- "$tmpGz"
-            fi
         done
     done
-
-    if [[ "$type" == 'sqlite' ]]; then
-        checkFileInTARPrefix '' tests/single-nested-file.tar foo/fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
-        checkFileInTARPrefix foo tests/single-nested-file.tar fighter/ufo 2709a3348eb2c52302a7606ecf5860bc
-        checkFileInTARPrefix foo/fighter tests/single-nested-file.tar ufo 2709a3348eb2c52302a7606ecf5860bc
-    fi
 done
 
 #benchmarkSerialization # takes quite long, and a benchmark is not a test ...
