01d16c1dd75456f6a0fd026235166c5f7e88674e
mxmlnkn <mxmlnkn@github.de>
2021-08-29 23:22:57 +0200

    [fix] Only install compression dependencies if possible on a particular platform

--- setup.py
+++ setup.py
@@ -31,15 +31,23 @@ setup(
     long_description_content_type = 'text/markdown',
 
     py_modules       = [ 'ratarmount' ],
-    install_requires = requirements + [ 'dataclasses; python_version < "3.7.0"' ],
+    install_requires = [ 'fusepy',
+                         'indexed_bzip2>=1.1.2; platform_system=="Linux"',
+                         'indexed_bzip2>=1.3.0; platform_system!="Linux"',
+                         'indexed_gzip>=1.5.3',
+                         'indexed_zstd>=1.3.1; sys_platform=="darwin"',
+                         'indexed_zstd>=1.2.2; platform_system!="Windows"',
+                         'dataclasses; python_version < "3.7.0"' ],
     # Make these optional requirements because they have no binaries on PyPI meaning they are built from source
     # and will fail if system dependencies are not installed.
     extras_require   = {
-                            'full' : [ 'cffi', 'lzmaffi' ],
+                            'full' : [ 'cffi; platform_system!="Windows"',
+                                       'lzmaffi; platform_system!="Windows"' ],
                             # cffi dependency seems to be configured wrong in lzmaffi,
                             # therefore also list it here before lzmaffi:
                             # https://github.com/r3m0t/backports.lzma/issues/3
-                            'xz' : [ 'cffi', 'lzmaffi' ],
+                            'xz' : [ 'cffi; platform_system!="Windows"',
+                                     'lzmaffi; platform_system!="Windows"' ],
                        },
     entry_points = { 'console_scripts': [ 'ratarmount=ratarmount:cli' ] }
 )
