6c3eef63f5d76c5632c4485a78c4546c6f3e9b8e
mxmlnkn <mxmlnkn@github.de>
2021-09-25 17:51:09 +0200

    [fix] Make indexed_zstd optional on macOS because of missing wheels

--- setup.py
+++ setup.py
@@ -37,8 +37,7 @@ setup(
         'fusepy',
         'indexed_bzip2>=1.3.0',
         'indexed_gzip>=1.6.3',
-        'indexed_zstd>=1.3.1; sys_platform=="darwin"',
-        'indexed_zstd>=1.2.2; platform_system!="Windows"',
+        'indexed_zstd>=1.2.2; platform_system=="Linux"',
         'dataclasses; python_version < "3.7.0"',
         'python-xz>=0.1.2',
         'rarfile>=4.0'
@@ -46,8 +45,13 @@ setup(
     # Make these optional requirements because they have no binaries on PyPI meaning they are built from source
     # and will fail if system dependencies are not installed.
     extras_require   = {
-        'full' : [],
-        'xz' : [],
+        'full'  : [ 'indexed_zstd>=1.3.1; sys_platform=="darwin"' ],
+        'bzip2' : [],
+        'gzip'  : [],
+        'rar'   : [],
+        'xz'    : [],
+        'zip'   : [],
+        'zstd'  : [ 'indexed_zstd>=1.3.1; sys_platform=="darwin"' ],
     },
     entry_points = { 'console_scripts': [ 'ratarmount=ratarmount:cli' ] }
 )
