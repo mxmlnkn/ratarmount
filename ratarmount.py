aaf11bd6adab8e693230544a1b03dab9c05f5021
mxmlnkn <mxmlnkn@github.de>
2021-09-25 17:19:53 +0200

    [fix] Manually check magic bytes because is_zipfile is too lenient

--- ratarmount.py
+++ ratarmount.py
@@ -2499,29 +2499,53 @@ def openMountSource(fileOrPath: Union[str, IO[bytes]], **options) -> MountSource
             return FolderMountSource('.' if fileOrPath == '.' else os.path.realpath(fileOrPath))
 
     try:
-        if zipfile.is_zipfile(fileOrPath):
-            return ZipMountSource(fileOrPath, **options)
-    except Exception as e:
+        if 'rarfile' in sys.modules and rarfile.is_rarfile(fileOrPath):
+            return RarMountSource(fileOrPath, **options)
+    except Exception as exception:
         if printDebug >= 1:
-            print("[Info] Checking for zip file raised an exception:", e)
+            print("[Info] Checking for RAR file raised an exception:", exception)
     finally:
         if hasattr(fileOrPath, 'seek'):
             fileOrPath.seek(0)  # type: ignore
 
     try:
-        if 'rarfile' in sys.modules and rarfile.is_rarfile(fileOrPath):
-            return RarMountSource(fileOrPath, **options)
-    except Exception as e:
+        if isinstance(fileOrPath, str):
+            return SQLiteIndexedTar(fileOrPath, **options)
+
+        return SQLiteIndexedTar(fileObject=fileOrPath, **options)
+    except RatarmountError as exception:
+        if printDebug >= 3:
+            print("[Info] Checking for (compressed) TAR file raised an exception:", exception)
+    except Exception as exception:
         if printDebug >= 1:
-            print("[Info] Checking for rar file raised an exception:", e)
+            print("[Info] Checking for (compressed) TAR file raised an exception:", exception)
+
+    try:
+        # is_zipfile is much too lax when testing for ZIPs because it's only testing for the central directory
+        # at the end of the file not the magic bits at the beginning. Meaning, if another non-ZIP archive has
+        # zip contents at the end, then it might get misclassified! Thefore, manually check for PK at start.
+        # https://bugs.python.org/issue16735
+        # https://bugs.python.org/issue28494
+        # https://bugs.python.org/issue42096
+        # https://bugs.python.org/issue45287
+        # TODO This will not recognize self-extracting ZIP archives, so for now, those are simply not supported!
+        if isinstance(fileOrPath, str):
+            with open(fileOrPath, 'rb') as file:
+                if supportedCompressions['zip'].checkHeader(file) and zipfile.is_zipfile(fileOrPath):
+                    return ZipMountSource(fileOrPath, **options)
+        else:
+            # TODO One problem here is when trying to read and then seek back but there also is no peek method.
+            #      https://github.com/markokr/rarfile/issues/73
+            if fileOrPath.read(2) == b'PK' and zipfile.is_zipfile(fileOrPath):
+                return ZipMountSource(fileOrPath, **options)
+    except Exception as exception:
+        if printDebug >= 1:
+            print("[Info] Checking for ZIP file raised an exception:", exception)
     finally:
         if hasattr(fileOrPath, 'seek'):
             fileOrPath.seek(0)  # type: ignore
 
-    if isinstance(fileOrPath, str):
-        return SQLiteIndexedTar(fileOrPath, **options)
-
-    return SQLiteIndexedTar(fileObject=fileOrPath, **options)
+    raise CompressionError("Archive to open has unrecognized format!")
 
 
 class UnionMountSource(MountSource):
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1187,6 +1187,10 @@ tests+=(
     2709a3348eb2c52302a7606ecf5860bc tests/file-in-non-existing-folder.rar        foo2/ufo
     2709a3348eb2c52302a7606ecf5860bc tests/folder-symlink.rar                     foo/fighter/ufo
     2709a3348eb2c52302a7606ecf5860bc tests/folder-symlink.rar                     foo/jet/ufo
+    # TODO not working because of https://github.com/markokr/rarfile/issues/73
+    #49b996b16f59ab6c87dea31e227f8798 tests/rar-misrecognized-as-zip.rar           bag.zip/README.md
+    #49b996b16f59ab6c87dea31e227f8798 tests/rar-misrecognized-as-zip.rar           bag.zip/CHANGELOG.md
+    #49b996b16f59ab6c87dea31e227f8798 tests/rar-misrecognized-as-zip.rar           bag1.zip/CHANGELOG.md
 
     d3b07384d113edec49eaa6238ad5ff00 tests/single-file.tar                        bar
     d3b07384d113edec49eaa6238ad5ff00 tests/single-file-with-leading-dot-slash.tar bar
