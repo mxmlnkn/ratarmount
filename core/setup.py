e13792a8fabc4ee8a92d4cf2bbd9996b2f40d2a1
mxmlnkn <mxmlnkn@github.de>
2021-10-11 23:51:28 +0200

    [build] Work around pip bug for editable install and pyproject.toml

--- core/setup.py
+++ core/setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import site
+import sys
 from setuptools import setup
+
+# workaround https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup()
