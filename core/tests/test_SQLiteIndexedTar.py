mxmlnkn/ratarmount

commit 65a7923e8c13c452cffcbb4cbc1c299a52dac23c

[fix] Write out index if a path is given, even if the TAR is just a file-like object

core/ratarmountcore/SQLiteIndexedTar.py
@@ -167,14 +167,9 @@ class SQLiteIndexedTar(MountSource):
             except Exception:
                 pass
 
-        if not tarFileName:
-            self._createIndex(self.tarFileObject)
-            # return here because we can't find a save location without any identifying name
-            return
-
         # will be used for storing indexes if current path is read-only
-        possibleIndexFilePaths = [self.tarFileName + ".index.sqlite"]
-        indexPathAsName = self.tarFileName.replace("/", "_") + ".index.sqlite"
+        possibleIndexFilePaths = [self.tarFileName + ".index.sqlite"] if self.tarFileName else []
+        indexPathAsName = self.tarFileName.replace("/", "_") + ".index.sqlite" if self.tarFileName else None
         if isinstance(indexFolders, str):
             indexFolders = [indexFolders]
 
@@ -189,7 +184,7 @@ class SQLiteIndexedTar(MountSource):
             if '' not in indexFolders:
                 possibleIndexFilePaths = []
             for folder in indexFolders:
-                if folder:
+                if folder and indexPathAsName:
                     indexPath = os.path.join(folder, indexPathAsName)
                     possibleIndexFilePaths.append(os.path.abspath(os.path.expanduser(indexPath)))
 

commit ef60177acd94b62a5771a83ab0b7a3df2308b825

[fix] Do not create <file object>.index.sqlite files
This bug was introduced in 65a7923.
Add tests to cover this and the original problem.

core/ratarmountcore/SQLiteIndexedTar.py
@@ -131,23 +131,26 @@ class SQLiteIndexedTar(MountSource):
         self.gzipSeekPointSpacing       = gzipSeekPointSpacing
         self.parallelization            = parallelization
         self.printDebug                 = printDebug
-        self.isCustomTarFileName        = fileObject is not None
+        self.isFileObject               = fileObject is not None
         self.isGnuIncremental           = isGnuIncremental
         # fmt: on
 
-        self.tarFileName: str = '<file object>'
-        if not fileObject:
-            if not tarFileName:
+        # Determine an archive file name to show for debug output
+        self.tarFileName: str
+        if fileObject:
+            self.tarFileName = tarFileName if tarFileName else '<file object>'
+        else:
+            if tarFileName:
+                self.tarFileName = os.path.abspath(tarFileName)
+            else:
                 raise ValueError("At least one of tarFileName and fileObject arguments should be set!")
-            self.tarFileName = os.path.abspath(tarFileName)
-            fileObject = open(self.tarFileName, 'rb')
-        elif tarFileName:
-            # If tarFileName was specified for a file object, set self.tarFileName accordingly.
-            self.tarFileName = tarFileName
 
+        # If no fileObject given, then self.tarFileName is the path to the archive to open.
+        if not fileObject:
+            fileObject = open(self.tarFileName, 'rb')
         fileObject.seek(0, io.SEEK_END)
         fileSize = fileObject.tell()
-        fileObject.seek(0)
+        fileObject.seek(0)  # Even if not interested in the file size, seeking to the start might be useful.
 
         # rawFileObject : Only set when opening a compressed file and only kept to keep the
         #                 compressed file handle from being closed by the garbage collector.
@@ -174,8 +177,13 @@ class SQLiteIndexedTar(MountSource):
                 pass
 
         # will be used for storing indexes if current path is read-only
-        possibleIndexFilePaths = [self.tarFileName + ".index.sqlite"] if self.tarFileName else []
-        indexPathAsName = self.tarFileName.replace("/", "_") + ".index.sqlite" if self.tarFileName else None
+        if self.isFileObject:
+            possibleIndexFilePaths = [':memory:']
+            indexPathAsName = None
+        else:
+            possibleIndexFilePaths = [self.tarFileName + ".index.sqlite"]
+            indexPathAsName = self.tarFileName.replace("/", "_") + ".index.sqlite"
+
         if isinstance(indexFolders, str):
             indexFolders = [indexFolders]
 
@@ -308,7 +316,7 @@ class SQLiteIndexedTar(MountSource):
         connection.executescript(metadataTable)
 
         # All of these require the generic "metadata" table.
-        if not self.isCustomTarFileName:
+        if not self.isFileObject:
             self._storeTarMetadata(connection, self.tarFileName, printDebug=self.printDebug)
         self._storeArgumentsMetadata(connection)
         connection.commit()

core/tests/test_SQLiteIndexedTar.py
@@ -78,23 +78,45 @@ class TestSQLiteIndexedTarParallelized:
 
     @staticmethod
     def test_index_creation_and_loading(parallelization):
-        with tempfile.NamedTemporaryFile(suffix=".bz2") as tmpTarFile:
-            contents = b"Hello World!"
-            with bz2.open(tmpTarFile.name, "wb") as bz2File:
-                bz2File.write(contents)
+        with tempfile.TemporaryDirectory() as tmpDirectory:
+            oldCurrentWorkingDirectory = os.getcwd()
+            os.chdir(tmpDirectory)
+            try:
+                archiveName = 'simple.bz2'
+                indexPath = 'simple.custom.index'
+                contents = b"Hello World!"
+                with bz2.open(archiveName, "wb") as bz2File:
+                    bz2File.write(contents)
 
-            testIndex = TestSQLiteIndexedTarParallelized._test_index_creation_and_loading
+                def testIndex(x, y, z):
+                    TestSQLiteIndexedTarParallelized._test_index_creation_and_loading(
+                        x, y, z, contents, parallelization
+                    )
 
-            with tempfile.NamedTemporaryFile(suffix=".sqlite") as tmpIndexFile:
-                testIndex(None, tmpTarFile.name, tmpIndexFile.name, contents, parallelization)
+                # Three optional arguments give 2^3=8 test cases
 
-            with tempfile.NamedTemporaryFile(suffix=".sqlite") as tmpIndexFile:
-                with open(tmpTarFile.name, "rb") as file:
-                    testIndex(file, "tarFileName", tmpIndexFile.name, contents, parallelization)
+                with pytest.raises(ValueError):
+                    testIndex(None, None, indexPath)
+                with pytest.raises(ValueError):
+                    testIndex(None, None, None)
+
+                testIndex(archiveName, None, indexPath)
+                testIndex(archiveName, None, None)
+
+                with open(archiveName, "rb") as file:
+                    testIndex("tarFileName", file, indexPath)
+                    testIndex("tarFileName", file, None)
+                    testIndex(None, file, indexPath)
+                    testIndex(None, file, None)
+
+            finally:
+                os.chdir(oldCurrentWorkingDirectory)
 
     @staticmethod
-    def _test_index_creation_and_loading(fileObject, tarFileName, indexFilePath, contents, parallelization):
-        assert not os.path.exists(indexFilePath) or os.stat(indexFilePath).st_size == 0
+    def _test_index_creation_and_loading(tarFileName, fileObject, indexFilePath, contents, parallelization):
+        if indexFilePath:
+            assert not os.path.exists(indexFilePath) or os.stat(indexFilePath).st_size == 0
+        oldFolderContents = os.listdir('.')
 
         # Create index
         with SQLiteIndexedTar(
@@ -107,7 +129,15 @@ class TestSQLiteIndexedTarParallelized:
         ):
             pass
 
-        assert os.stat(indexFilePath).st_size > 0
+        # When opening a file object without a path, no index should have been created anywhere!
+        if indexFilePath is None and fileObject is not None:
+            assert oldFolderContents == os.listdir('.')
+            return
+
+        createdIndexFilePath = indexFilePath
+        if not indexFilePath and not fileObject and tarFileName:
+            createdIndexFilePath = tarFileName + '.index.sqlite'
+        assert os.stat(createdIndexFilePath).st_size > 0
 
         # Read from index
         indexedFile = SQLiteIndexedTar(
@@ -119,16 +149,20 @@ class TestSQLiteIndexedTarParallelized:
             parallelization=parallelization,
         )
 
-        expected_name = os.path.basename(tarFileName).rsplit('.', 1)[0] if fileObject is None else tarFileName
+        objectName = '<file object>' if tarFileName is None else tarFileName
+        expectedName = os.path.basename(tarFileName).rsplit('.', 1)[0] if fileObject is None else objectName
 
         finfo = indexedFile._getFileInfo("/", listDir=True)
-        assert expected_name in finfo
-        assert finfo[expected_name].size == len(contents)
+        assert expectedName in finfo
+        assert finfo[expectedName].size == len(contents)
 
-        finfo = indexedFile.getFileInfo("/" + expected_name)
+        finfo = indexedFile.getFileInfo("/" + expectedName)
         assert finfo.size == len(contents)
         assert indexedFile.read(finfo, size=len(contents), offset=0) == contents
         assert indexedFile.read(finfo, size=3, offset=3) == contents[3:6]
+
+        if createdIndexFilePath:
+            os.remove(createdIndexFilePath)
 
     @staticmethod
     def test_listDir_and_fileVersions(parallelization):