9721eadf091aaa13e99f42e662b80a09204e35c8
Vadim Lebedev <vlebedev@orangecyberdefense.com>
2022-07-17 15:24:20 +0200

    Synatx error fix

--- core/ratarmountcore/factory.py
+++ core/ratarmountcore/factory.py
@@ -43,7 +43,7 @@ def openMountSource(fileOrPath: Union[str, IO[bytes]], **options) -> MountSource
             )
 
     special_formats = ('zip', 'tar', 'rar') if not options.get('force_libarchive', False) else (None, )
-    if printDebug > 0 and options.get('force_libarchive', False);
+    if printDebug > 0 and options.get('force_libarchive', False):
         print("[Info] .zip, .tar,  and .rar will be handled by libarchive.") 
 
     try:
