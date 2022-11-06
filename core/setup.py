2b5b71ae56e10c4d0272b9c18bbf18d8d40dc0cb
mxmlnkn <mxmlnkn@github.de>
2022-11-06 22:30:10 +0100

    [doc] Add workaround and solution for problem with editable install

--- core/setup.py
+++ core/setup.py
@@ -10,5 +10,9 @@ site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 # Workaround for https://github.com/pypa/setuptools/issues/2938
 #   Does not work: python3 -m pip install --user --editable
 #   Use: python3 setup.py develop --user
-
+# Workaround for https://github.com/pypa/setuptools/issues/3582#issuecomment-1242384337
+#   python3 -m pip install --user --no-build-isolation --editable
+# https://github.com/pypa/pip/issues/6264#issuecomment-1184327415
+# https://github.com/pypa/pip/pull/11466
+# Upgrade to pip 22.3.1 or higher to avoid having to specify --no-build-isolation
 setup()
