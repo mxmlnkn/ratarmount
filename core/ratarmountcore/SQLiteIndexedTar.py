mxmlnkn/ratarmount

commit ced90ccd89d6cf5c54c5b5dace041279118985f3

[API] Restructure supported compression format list

core/ratarmountcore/SQLiteIndexedTar.py
@@ -38,7 +38,7 @@ from .MountSource import FileInfo, MountSource
 from .ProgressBar import ProgressBar
 from .SQLiteBlobFile import SQLiteBlobsFile, WriteSQLiteBlobs
 from .StenciledFile import StenciledFile
-from .compressions import getGzipInfo, supportedCompressions
+from .compressions import findAvailableOpen, getGzipInfo, TAR_COMPRESSION_FORMATS
 from .utils import (
     RatarmountError,
     IndexNotOpenError,
@@ -970,11 +970,15 @@ class SQLiteIndexedTar(MountSource):
                 makeVersionRow('index', SQLiteIndexedTar.__version__),
             ]
 
-            for _, cinfo in supportedCompressions.items():
-                if cinfo.moduleName in sys.modules:
-                    moduleVersion = findModuleVersion(sys.modules[cinfo.moduleName])
-                    if moduleVersion:
-                        versions += [makeVersionRow(cinfo.moduleName, moduleVersion)]
+            for moduleName in set(
+                module.name
+                for _, info in TAR_COMPRESSION_FORMATS.items()
+                for module in info.modules
+                if module.name in sys.modules
+            ):
+                moduleVersion = findModuleVersion(sys.modules[moduleName])
+                if moduleVersion:
+                    versions += [makeVersionRow(moduleName, moduleVersion)]
 
             connection.executemany('INSERT OR REPLACE INTO "versions" VALUES (?,?,?,?,?)', versions)
         except Exception as exception:
@@ -2086,10 +2090,7 @@ class SQLiteIndexedTar(MountSource):
             return None
 
         oldOffset = fileobj.tell()
-        for compressionId, compression in supportedCompressions.items():
-            if compressionId in ['rar', 'zip']:
-                continue
-
+        for compressionId, compression in TAR_COMPRESSION_FORMATS.items():
             # The header check is a necessary condition not a sufficient condition.
             # Especially for gzip, which only has 2 magic bytes, false positives might happen.
             # Therefore, only use the magic bytes based check if the module could not be found
@@ -2099,11 +2100,12 @@ class SQLiteIndexedTar(MountSource):
             if not matches:
                 continue
 
-            if compression.moduleName not in sys.modules and matches:
+            formatOpen = findAvailableOpen(compressionId)
+            if formatOpen:
                 return compressionId
 
             try:
-                compressedFileobj = compression.open(fileobj)
+                compressedFileobj = formatOpen(fileobj)
                 # Reading 1B from a single-frame zst file might require decompressing it fully in order
                 # to get uncompressed file size! Avoid that. The magic bytes should suffice mostly.
                 # TODO: Make indexed_zstd not require the uncompressed size for the read call.
@@ -2150,13 +2152,15 @@ class SQLiteIndexedTar(MountSource):
         if printDebug >= 3:
             print(f"[Info] Detected compression {compression} for file object:", fileobj)
 
-        if compression not in supportedCompressions:
+        if compression not in TAR_COMPRESSION_FORMATS:
             return fileobj, None, compression, SQLiteIndexedTar._detectTar(fileobj, encoding, printDebug=printDebug)
 
-        cinfo = supportedCompressions[compression]
-        if cinfo.moduleName not in sys.modules:
+        formatOpen = findAvailableOpen(compression)
+        if not formatOpen:
+            moduleNames = [module.name for module in TAR_COMPRESSION_FORMATS[compression].modules]
             raise CompressionError(
-                f"Can't open a {compression} compressed file '{fileobj.name}' without {cinfo.moduleName} module!"
+                f"Cannot open a {compression} compressed TAR file '{fileobj.name}' "
+                f"without any of these modules: {moduleNames}"
             )
 
         if compression == 'gz':
@@ -2181,12 +2185,12 @@ class SQLiteIndexedTar(MountSource):
             and os.path.isfile(fileobj.name)
             and platform.system() == 'Linux'
         ):
-            tar_file = cinfo.open(fileobj)
+            tar_file = formatOpen(fileobj)
             if len(tar_file.block_boundaries) > 1:
                 tar_file.close()
                 tar_file = ParallelXZReader(fileobj.name, parallelization=parallelization)
         else:
-            tar_file = cinfo.open(fileobj)
+            tar_file = formatOpen(fileobj)
 
         return tar_file, fileobj, compression, SQLiteIndexedTar._detectTar(tar_file, encoding, printDebug=printDebug)
 

core/ratarmountcore/__init__.py
@@ -42,7 +42,17 @@ Example:
 
 from .version import __version__
 
-from .compressions import supportedCompressions, stripSuffixFromTarFile, checkForSplitFile, compressZstd, getGzipInfo
+from .compressions import (
+    TAR_COMPRESSION_FORMATS,
+    ARCHIVE_FORMATS,
+    findAvailableOpen,
+    supportedCompressions,
+    stripSuffixFromTarFile,
+    checkForSplitFile,
+    compressZstd,
+    getGzipInfo,
+)
+
 from .utils import (
     RatarmountError,
     IndexNotOpenError,

core/ratarmountcore/compressions.py
@@ -6,7 +6,7 @@ import concurrent.futures
 import os
 import struct
 import sys
-from typing import Callable, IO, Iterable, List, Optional, Tuple
+from typing import Callable, Dict, IO, Iterable, List, Optional, Tuple
 
 from .utils import isLatinAlpha, isLatinDigit, isLatinHexAlpha, formatNumber, ALPHA, DIGITS, HEX
 
@@ -58,56 +58,81 @@ else:
     zipfile = None
 
 
+CompressionModuleInfo = collections.namedtuple('CompressionModuleInfo', ['name', 'open'])
 # Defining lambdas does not yet check the names of entities used inside the lambda!
-CompressionInfo = collections.namedtuple(
-    'CompressionInfo', ['suffixes', 'doubleSuffixes', 'moduleName', 'checkHeader', 'open']
-)
+# "modules" contains a list of CompressionModuleInfo for modules that are available.
+# Those appearing first in this list have priority.
+CompressionInfo = collections.namedtuple('CompressionInfo', ['suffixes', 'doubleSuffixes', 'modules', 'checkHeader'])
 
 
-supportedCompressions = {
+TAR_COMPRESSION_FORMATS: Dict[str, CompressionInfo] = {
     'bz2': CompressionInfo(
         ['bz2', 'bzip2'],
         ['tb2', 'tbz', 'tbz2', 'tz2'],
-        'indexed_bzip2',
+        [CompressionModuleInfo('indexed_bzip2', lambda x: indexed_bzip2.open(x))],
         lambda x: (x.read(4)[:3] == b'BZh' and x.read(6) == (0x314159265359).to_bytes(6, 'big')),
-        lambda x: indexed_bzip2.open(x),
     ),
     'gz': CompressionInfo(
         ['gz', 'gzip'],
         ['taz', 'tgz'],
-        'indexed_gzip',
+        [CompressionModuleInfo('indexed_gzip', lambda x: indexed_gzip.IndexedGzipFile(fileobj=x))],
         lambda x: x.read(2) == b'\x1F\x8B',
-        lambda x: indexed_gzip.IndexedGzipFile(fileobj=x),
-    ),
-    'rar': CompressionInfo(
-        ['rar'],
-        [],
-        'rarfile',
-        lambda x: x.read(6) == b'Rar!\x1A\x07',
-        lambda x: rarfile.RarFile(x),
     ),
     'xz': CompressionInfo(
         ['xz'],
         ['txz'],
-        'lzmaffi' if 'lzmaffi' in sys.modules else 'xz',
+        # Prioritize xz over lzmaffi
+        [
+            CompressionModuleInfo('xz', lambda x: xz.open(x)),
+            CompressionModuleInfo('lzmaffi', lambda x: lzmaffi.open(x)),
+        ],
         lambda x: x.read(6) == b"\xFD7zXZ\x00",
-        (lambda x: lzmaffi.open(x)) if 'lzmaffi' in sys.modules else (lambda x: xz.open(x)),
-    ),
-    'zip': CompressionInfo(
-        ['zip'],
-        [],
-        'zipfile',
-        lambda x: x.read(2) == b'PK',
-        lambda x: zipfile.ZipFile(x),
     ),
     'zst': CompressionInfo(
         ['zst', 'zstd'],
         ['tzst'],
-        'indexed_zstd',
+        [CompressionModuleInfo('indexed_zstd', lambda x: indexed_zstd.IndexedZstdFile(x.fileno()))],
         lambda x: x.read(4) == (0xFD2FB528).to_bytes(4, 'little'),
-        lambda x: indexed_zstd.IndexedZstdFile(x.fileno()),
     ),
 }
+
+
+ARCHIVE_FORMATS: Dict[str, CompressionInfo] = {
+    'rar': CompressionInfo(
+        ['rar'],
+        [],
+        [CompressionModuleInfo('rarfile', lambda x: rarfile.RarFile(x))],
+        lambda x: x.read(6) == b'Rar!\x1A\x07',
+    ),
+    'zip': CompressionInfo(
+        ['zip'],
+        [],
+        [CompressionModuleInfo('zipfile', lambda x: zipfile.ZipFile(x))],
+        lambda x: x.read(2) == b'PK',
+    ),
+}
+
+
+supportedCompressions = {**TAR_COMPRESSION_FORMATS, **ARCHIVE_FORMATS}
+
+
+def findAvailableOpen(compression: str, prioritizedBackends: Optional[List[str]] = None) -> Optional[Callable]:
+    if compression not in supportedCompressions:
+        return None
+
+    modules = supportedCompressions[compression].modules
+    if prioritizedBackends:
+        for moduleName in prioritizedBackends:
+            if moduleName in sys.modules:
+                for module in modules:
+                    if module.name == moduleName and module.open:
+                        return module.open
+
+    for module in modules:
+        if module.name in sys.modules:
+            return module.open
+
+    return None
 
 
 def stripSuffixFromCompressedFile(path: str) -> str:

ratarmount.py
@@ -39,6 +39,7 @@ from ratarmountcore import (
     SQLiteIndexedTar,
     UnionMountSource,
     findModuleVersion,
+    findAvailableOpen,
     openMountSource,
     overrides,
     supportedCompressions,
@@ -817,10 +818,14 @@ def checkInputFileType(
 
         try:
             # Determining if there are many frames in zstd is O(1) with is_multiframe
-            if compression != 'zst' or supportedCompressions[compression].moduleName not in sys.modules:
+            if compression != 'zst':
                 raise Exception()  # early exit because we catch it anyways
 
-            zstdFile = supportedCompressions[compression].open(fileobj)
+            formatOpen = findAvailableOpen(compression)
+            if not formatOpen:
+                raise Exception()  # early exit because we catch it anyways
+
+            zstdFile = formatOpen(fileobj)
 
             if not zstdFile.is_multiframe() and fileSize > 1024 * 1024:
                 print(f"[Warning] The specified file '{tarFile}'")
@@ -845,10 +850,11 @@ def checkInputFileType(
 
             raise argparse.ArgumentTypeError(f"Archive '{tarFile}' can't be opened!")
 
-    cinfo = supportedCompressions[compression]
-    if cinfo.moduleName not in sys.modules:
+    if not findAvailableOpen(compression):
+        moduleNames = [module.name for module in supportedCompressions[compression].modules]
         raise argparse.ArgumentTypeError(
-            f"Can't open a {compression} compressed TAR file '{fileobj.name}' without {cinfo.moduleName} module!"
+            f"Cannot open a {compression} compressed TAR file '{fileobj.name}' "
+            f"without any of these modules: {moduleNames}"
         )
 
     return tarFile, compression
@@ -905,8 +911,9 @@ class PrintVersionAction(argparse.Action):
                 if moduleVersion:
                     print(moduleName, moduleVersion)
 
-        for _, cinfo in supportedCompressions.items():
-            printModuleVersion(cinfo.moduleName)
+        for _, info in supportedCompressions.items():
+            for module in info.modules:
+                printModuleVersion(module.name)
 
         sys.exit(0)
 

commit 54381ad00c28ac4570d1977a3f157a2aaac2a710

[fix] Improve error message when a file cannot be read because of a missing dependency
The current error on -d 3 would be "NoneType is not callable", which
isn't helpful at all. This worked better before. The bug got introduced
in ced90ccd.

core/ratarmountcore/SQLiteIndexedTar.py
@@ -1422,8 +1422,15 @@ class SQLiteIndexedTar(MountSource):
                 continue
 
             formatOpen = findAvailableOpen(compressionId, prioritizedBackends)
-            if formatOpen:
-                return compressionId
+            # If no appropriate module exists, then don't do any further checks.
+            if not formatOpen:
+                if printDebug >= 1:
+                    print(
+                        f"[Warning] A given file with magic bytes for {compressionId} could not be opened because "
+                        "no appropriate Python module could be loaded. Are some dependencies missing? To install "
+                        "ratarmountcore with all dependencies do: python3 -m pip install --user ratarmountcore[full]"
+                    )
+                return None
 
             try:
                 compressedFileobj = formatOpen(fileobj)