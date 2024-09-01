efeb8c91033c4f101bbcecdd12fe6921ddfa34db
mxmlnkn <mxmlnkn@github.de>
2024-09-02 00:32:43 +0200

    [build] AppImage: Source the Python-AppImage Python wrapper script to avoid invoking bash after changing LD_LIBRARY_PATH
    So many hours, which could have been spent on new features or
    performance improvements, were lost in trying to fix these few lines of
    incantation to avoid the bash warning about:
    
        libtinfo.so.6: no version information available
    
    I do not even know where to file a bug. It worked with manylinux2014, so
    maybe file a bug with manylinux? Or is AlmaLinux 8 at fault, or
    libcurses for bundling some of its shared libraries without version
    information?

--- AppImage/ratarmount-metadata/entrypoint.sh
+++ AppImage/ratarmount-metadata/entrypoint.sh
@@ -9,5 +9,23 @@ if [[ -f "${APPDIR}/usr/lib/libarchive.so.13" ]]; then
     export LIBARCHIVE="${APPDIR}/usr/lib/libarchive.so.13"
 fi
 
+# Avoid calling a subprocess bash after changing LD_LIBRARY_PATH in order to avoid the warning:
+#     libtinfo.so.6: no version information available
 # -u is important or else piping the output to other tools and therefore the tests might fail!
-{{ python-executable }} -u -I -m ratarmount "$@"
+ARGS=( -u -I -m ratarmount "$@" )
+
+# Also avoid calling external processes like "head" or "grep" because they would have to be bundled.
+# Try to use bash built-in's only:
+#     https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+#     https://www.gnu.org/software/bash/manual/html_node/Bourne-Shell-Builtins.html
+#     https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+read -r firstLine 2>/dev/null < {{ python-executable }}
+if [[ "$firstLine" =~ ^#\!.*bash$ ]]; then
+    # Sourcing the file avoids the shebang being interpreted and another bash process being started.
+    # However, this means that this Python wrapper script, which checks $0 to get its location, is wrong
+    # and needs to be patched to use BASH_SOURCE.
+    BASH_ARGV0={{ python-executable }}
+    . {{ python-executable }} "${ARGS[@]}"
+else
+    {{ python-executable }} "${ARGS[@]}"
+fi
