faabc916b233de775a5d9b2b31f8c4ea01d337f1
mxmlnkn <mxmlnkn@github.de>
2024-10-12 12:21:04 +0200

    [test] Work around random "Failed to establish a new connection" errors

--- tests/wait-for-port.py
+++ tests/wait-for-port.py
@@ -12,9 +12,12 @@ t0 = time.time()
 with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
     n = max(1, int(math.ceil(timeout / 5)))
     for i in range(n):
-        if sock.connect_ex(("127.0.0.1", int(sys.argv[1]))) == 0:
-            print(f"Weed port opened after {time.time() - t0:.1f} s.")
-            sys.exit(0)
+        try:
+            if sock.connect_ex(("127.0.0.1", int(sys.argv[1]))) == 0:
+                print(f"Weed port opened after {time.time() - t0:.1f} s.")
+                sys.exit(0)
+        except Exception:
+            pass
         # Do not wait after the last test.
         if i < n - 1:
             time.sleep(5)
