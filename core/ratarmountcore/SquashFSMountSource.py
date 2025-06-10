9909b573e703e25a7cbbf11721b04ef8cf996345
mxmlnkn <mxmlnkn@github.de>
2025-06-10 22:20:48 +0200

    [wip][feature] Make it possible to specify list of transformation rules
     - [ ] Add option for list of transform rules
        -> beware, this should not trigger index metadata checks when it compares a list with a single tuple vs. a tuple...
     - [ ] Apply transform recursive mount point to SubvolumesMountSource
      -> Move transform pattern into SQLiteIndex
      -> Somehow move metadata check and store into index, most trouble will be SQLiteIndexedTar,
         - maybe somehow give argumentsToCheck as a parameter to the index
         - Avoid giving self to index for tuples and garbage collection! A callback that stores self is the same!
         - Maybe also give a dict of parameters?
         - Avoid explicit argument forwarding and simply forward options from MountSource to Index?

--- core/ratarmountcore/SquashFSMountSource.py
+++ core/ratarmountcore/SquashFSMountSource.py
@@ -401,7 +401,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
         verifyModificationTime : bool                      = False,
         printDebug             : int                       = 0,
         indexMinimumFileCount  : int                       = 1000,
-        transform              : Optional[Tuple[str, str]] = None,
+        transform              : TransformPatterns         = None,
         **options
         # fmt: on
     ) -> None:
@@ -427,14 +427,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
         self.verifyModificationTime = verifyModificationTime
         self.printDebug             = printDebug
         self.options                = options
-        self.transformPattern       = transform
         # fmt: on
-
-        self.transform = (
-            (lambda x: re.sub(self.transformPattern[0], self.transformPattern[1], x))
-            if isinstance(self.transformPattern, (tuple, list)) and len(self.transformPattern) == 2
-            else (lambda x: x)
-        )
 
         super().__init__(
             SQLiteIndex(
@@ -444,6 +437,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
                 encoding=self.encoding,
                 printDebug=self.printDebug,
                 indexMinimumFileCount=indexMinimumFileCount,
+                transform=transform,
                 backendName='SquashFSMountSource',
             ),
             clearIndexCache=clearIndexCache,
@@ -472,7 +466,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
 
     def _storeMetadata(self) -> None:
         argumentsToSave = ['encoding', 'transformPattern']
-        argumentsMetadata = json.dumps({argument: getattr(self, argument) for argument in argumentsToSave})
+        argumentsMetadata = json.dumps({argument: getattr(self.index, argument) for argument in argumentsToSave})
         self.index.storeMetadata(argumentsMetadata, self.archiveFilePath)
 
     def _convertToRow(self, inodeOffset: int, info: "PySquashfsImage.file.File") -> Tuple:  # type: ignore
@@ -485,7 +479,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
             # Note that PySquashfsImage.file.Directory inherits from file.File, i.e., info can also be a directory.
             mode = mode | (stat.S_IFDIR if info.is_dir else stat.S_IFREG)
 
-        path, name = SQLiteIndex.normpath(self.transform(info.path)).rsplit("/", 1)
+        path, name = SQLiteIndex.normpath(self.index.transformPath(info.path)).rsplit("/", 1)
 
         # Currently unused. Squashfs files are stored in multiple blocks, so a single offset is insufficient.
         dataOffset = 0
@@ -598,7 +592,7 @@ class SquashFSMountSource(SQLiteIndexMountSource):
 
         if 'arguments' in metadata:
             SQLiteIndex.checkMetadataArguments(
-                json.loads(metadata['arguments']), self, argumentsToCheck=['encoding', 'transformPattern']
+                json.loads(metadata['arguments']), self.index, argumentsToCheck=['encoding', 'transformPattern']
             )
 
         if 'backendName' not in metadata:
