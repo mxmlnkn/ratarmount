ec0e0863231eba18fd9c8690b5ff8ce241b477ce
mxmlnkn <mxmlnkn@github.de>
2025-07-06 10:00:48 +0200

    [build] Update ratarmountcore setuptools requirement

--- tests/clean.sh
+++ tests/clean.sh
@@ -3,7 +3,7 @@
 # Will print warnings for each folder because find tries to descend into it after finding those.
 # It may be confusing but it is a nice verbose output to list deleted folders.
 find . -type d '(' \
-    -name '*.egg-info' -or -name '*.mypy_cache' -or -name '__pycache__' -or \
+    -name '*.egg-info' -or -name '*.mypy_cache' -or -name '__pycache__' -or '.ruff_cache' -or \
     -name '*.pytest_cache' -or -name '*.pytype' -or -name 'dist' -or -name 'build' \
 ')' -exec rm -rf {} ';'
 'rm' -f httpd-ruby-webrick.log ratarmount.stderr.log.tmp ratarmount.stdout.log.tmp
