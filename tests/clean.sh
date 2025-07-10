8e179b5df2847b103a5ab4ca1fb5cf6909a19afb
mxmlnkn <mxmlnkn@github.de>
2025-07-10 13:52:29 +0200

    [build] Clean logged stdout and stderr from tests

--- tests/clean.sh
+++ tests/clean.sh
@@ -3,8 +3,9 @@
 # Will print warnings for each folder because find tries to descend into it after finding those.
 # It may be confusing but it is a nice verbose output to list deleted folders.
 find . -type d '(' \
-    -name '*.egg-info' -or -name '*.mypy_cache' -or -name '__pycache__' -or '.ruff_cache' -or \
-    -name '*.pytest_cache' -or -name '*.pytype' -or -name 'dist' -or -name 'build' \
+    -name '*.egg-info' -or -name '*.mypy_cache' -or -name '__pycache__' -or -name '.ruff_cache' -or \
+    -name '*.pytest_cache' -or -name '*.pytype' -or -name 'dist' -or -name 'build' -or \
+    -name 'ratarmount.stdout' -or -name 'ratarmount.stderr' \
 ')' -exec rm -rf {} ';'
 'rm' -f httpd-ruby-webrick.log ratarmount.stderr.log.tmp ratarmount.stdout.log.tmp
 
