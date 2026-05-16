mxmlnkn/ratarmount

commit acc8a3393d630337749d7aaf217f57e3f8d3aace

[fix] Avoid multiple archives overwriting the same index when combining --recursive with --index-file-path

core/ratarmountcore/mountsource/compositing/automount.py
@@ -209,6 +209,10 @@ class AutoMountLayer(MountSource):
             if joinedFile:
                 mountSource: MountSource = SingleFileMountSource(os.path.split(mountPoint)[1], joinedFile)
             elif isinstance(deepestMountSource, FolderMountSource):
+                # Remove indexFilePath argument from options because all recursive archives would try to open
+                # the index intended for the parent archive / folder.
+                if 'indexFilePath' in options:
+                    del options['indexFilePath']
                 # Open from file path on host file system in order to write out TAR index files.
                 # Care has to be taken if a folder is bind mounted onto itself because then it can happen that
                 # the file open triggers a recursive FUSE call, which then hangs up everything.

commit 53289766a6baa878a5f3b4aa642857b8797ae1cd

[fix] Restore --index-file :memory: functionality combined with --recursive <folder>
This broke with acc8a33 2026-05-10:

    [fix] Avoid multiple archives overwriting the same index when combining --recursive with --index-file-path

core/ratarmountcore/mountsource/compositing/automount.py
@@ -210,7 +210,7 @@ class AutoMountLayer(MountSource):
             elif (get_file_path := getattr(deepestMountSource, 'get_file_path', None)) and callable(get_file_path):
                 # Remove indexFilePath argument from options because all recursive archives would try to open
                 # the index intended for the parent archive / folder.
-                if 'indexFilePath' in options:
+                if 'indexFilePath' in options and options['indexFilePath'] != ':memory:':
                     del options['indexFilePath']
                 # Open from file path on host file system in order to write out TAR index files.
                 # Care has to be taken if a folder is bind mounted onto itself because then it can happen that